6b92c062b7ef72dd1d06ef333b1d4afb907731fa
Luis David <luisdavidbucio@hotmail.com>
2020-11-27 00:35:17 -0600

    error

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/api', (req, res) => {
 app.post('/api/users', async function (req, res) {
     let newUser = req.body;
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type,x-auth-user");
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
         // Validar si vienen las propiedades
         if(!newUser.nombre || !newUser.apellido || !newUser.correo || !newUser.sexo || !newUser.fecha || !newUser.password) {
