fe81aecbcbee86f1401d40062384317259b3d8f2
Luis David <luisdavidbucio@hotmail.com>
2020-11-27 00:53:53 -0600

    error

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    status_header(200);
     next();
   });
 
