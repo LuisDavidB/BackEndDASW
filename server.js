29c91d8b9c21e4081297668a37dbb312d7fb54e2
Luis David <luisdavidbucio@hotmail.com>
2020-11-28 00:34:23 -0600

    tarado

--- server.js
+++ server.js
@@ -139,10 +139,9 @@ app.get('/api/users/:email',function (req,res) {
       });
 });
 app.put('/api/users/:email',function (req,res) {
-    res.send();
-    /*let email= req.params.email;
     let newUser=req.body;
-    let sameEmailUser = await Users.find({correo: newUser.correo});
+    res.send(newUser);
+    /*let sameEmailUser = await Users.find({correo: newUser.correo});
     let sameNameUser = await Users.find({nombre: newUser.nombre, apellido: newUser.apellido});
     if(sameEmailUser.length > 0) {
         res.statusCode = 400;
