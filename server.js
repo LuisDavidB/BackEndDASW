f739a28a91ce44df234a6e2ca11e99c13fec635f
Luis David <luisdavidbucio@hotmail.com>
2020-11-28 00:49:22 -0600

    tarado

--- server.js
+++ server.js
@@ -143,15 +143,14 @@ app.put('/api/users/:email',function (req,res) {
     if(!newUser.nombre || !newUser.apellido || !newUser.correo || !newUser.sexo || !newUser.fecha || !newUser.password) {
         res.statusCode = 400;
         res.send('Las propiedades requeridas son: nombre, apellido, correo, sexo, fecha y passwor');
-    }else {
-        res.send(newUser);
-    }/*
+    }
     else {
         // Validar si existe un usuario con el mismo correo o nombres y apellidos
         let sameEmailUser = await Users.find({correo: newUser.correo});
         let sameNameUser = await Users.find({nombre: newUser.nombre, apellido: newUser.apellido});
-
-        if(sameEmailUser.length > 0) {
+        res.statusCode=202;
+        res.send("funca");
+        /*if(sameEmailUser.length > 0) {
             res.statusCode = 400;
             res.send('Ya existe un usuario con el mismo correo');
         }
