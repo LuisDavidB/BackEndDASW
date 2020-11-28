868cf6949f3097241dd20a88804a5e7e5b2bf10c
Luis David <luisdavidbucio@hotmail.com>
2020-11-28 00:28:44 -0600

    tarado

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/api/users/:email',function (req,res) {
 app.PUT('/api/users/:email',function (req,res) {
     let email= req.params.email;
     let newUser=req.body;
-    let sameEmailUser = await Users.find({correo: newUser.correo});
+   /* let sameEmailUser = await Users.find({correo: newUser.correo});
     let sameNameUser = await Users.find({nombre: newUser.nombre, apellido: newUser.apellido});
     if(sameEmailUser.length > 0) {
         res.statusCode = 400;
@@ -151,7 +151,7 @@ app.PUT('/api/users/:email',function (req,res) {
         res.statusCode = 400;
         res.send('Ya existe un usuario con el mismo nombre');
     }
-    else {
+    else {*/
         Users.findOneAndUpdate({correo:req.params.email},{$set:{"nombre":newUser.nombre}}, function(err, result) {
             if (result==null){
                 res.statusCode =400;
